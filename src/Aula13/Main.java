548c09886a970d327fd6714243dbc2b3b9f6eac2
LucielVitorBS <luciel.vitor.bs@gmail.com>
2018-10-28 00:49:01 -0300

    Enviando meus trabalhos

--- src/Aula13/Main.java
+++ src/Aula13/Main.java
@@ -1,5 +1,26 @@
 package Aula13;
 
+import java.util.ArrayList;
+
 public class Main {
 
+    public static void main(String[] args) {
+        ContaCorrente douglas= new ContaCorrente();
+        douglas.ler();
+        ContaCorrente julia = new ContaCorrente();
+        julia.ler();
+
+        ArrayList<ContaCorrente> banco= new ArrayList<>();
+
+//        banco.add(douglas);
+//        banco.add(0,julia);
+//        banco.remove(douglas);
+//        banco.remove(0);
+//        banco.size();
+        banco.contains(julia);
+        banco.get(0).getTitular().equals(julia.getTitular());
+//        banco.indexOf(julia);
+//        banco.isEmpty();
+
+    }
 }

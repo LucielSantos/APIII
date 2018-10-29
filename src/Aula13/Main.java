c7b34dcfb8f980548302e2cf398cf852efe52fd3
LucielVitorBS <luciel.vitor.bs@gmail.com>
2018-10-29 07:42:59 -0300

    Enviando meus trabalhos

--- src/Aula13/Main.java
+++ src/Aula13/Main.java
@@ -9,17 +9,70 @@ public class Main {
 
         ArrayList<ContaCorrente> lista= new ArrayList<>();
 
+        String resposta="";
+
+        while (!resposta.equals("0")){
+            System.out.println("Menu do Programa: \n" +
+                    "1- Criar Conta\n" +
+                    "2- Mostrar Saldo\n" +
+                    "3- Sacar\n" +
+                    "4- Depositar\n" +
+                    "5- Fazer Transferência\n" +
+                    "0- Sair");
+
+            System.out.print("Digite o número correspondente a operação: ");
+            resposta=conta.tc.next();
+
+            switch (resposta){
+                case "1":
+                    ContaCorrente aux= new ContaCorrente();
+                    aux.ler();
+                    int tam= lista.size()-1;
+                    boolean verif=true;
+                    while (tam>=0){
+                        verif=aux.getTitular().equals(lista.get(tam).getTitular());
+                        tam--;
+                    }
+                    if (!verif) {
+                        lista.add(aux);
+                    }else {
+                        System.out.println("ja criada");
+                    }
+                    break;
+
+            }
+
+        }
+
+
         for (int i=0;i<2;i++){
             ContaCorrente aux= new ContaCorrente();
             aux.ler();
-            int tam= lista.size()-1;
-            while (tam>=0){
-                System.out.println(aux.getTitular().equals(lista.get(tam).getTitular()));
-                tam--;
-            }
             lista.add(aux);
         }
-            System.out.println(lista.toString());
+
+        System.out.println(lista.toString());
+
+        System.out.println(lista.get(0).tansferecia(1000, lista.get(1)));
+
+        System.out.println(lista.toString());
+
+//        for (int i=0;i<2;i++){
+//            ContaCorrente aux= new ContaCorrente();
+//            aux.ler();
+//            int tam= lista.size()-1;
+//            boolean cu=true;
+//            while (tam>=0){
+//                cu=aux.getTitular().equals(lista.get(tam).getTitular());
+//                tam--;
+//            }
+//            if (!cu) {
+//                lista.add(aux);
+//            }else {
+//                System.out.println("ja criada");
+//            }
+//        }
+//            System.out.println(lista.toString());
 
 
 //        lista.add(douglas);
